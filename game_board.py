069e3de18e7fe9b39c186c939e3f9b481d763d01
Jake Snow <jsnow@videoamp.com>
2024-03-14 11:32:06 -0400

    Initial project files

---
+++ game_board.py
@@ -0,0 +1,173 @@
+import configparser
+import numpy as np
+import logging
+import random
+
+
+class GameBoard:
+
+    def __init__(self, logger = None):
+
+        self.config = configparser['Settings']
+
+        self.board = np.zeros((4,4), np.int16)
+        self.new_number_generation()
+
+        self.color_codes = self.config['GAME_COLORS']
+
+        self.logger = logger
+        if not self.logger:
+            self.logger = logging.getLogger("2048_logger")
+            self.logger.setLevel(logging.INFO)
+
+
+    def __str__(self):
+
+        """
+        Print the board
+        """
+
+        board_string = ""
+        for i in self.board:
+            board_string += "\n   -    -    -    -  \n"
+            row_str = "|"
+            for j in i:
+                square_str = str(j)
+                num_digits = len(square_str)
+                if num_digits == 1:
+                    before = "  "
+                    after = "  "
+                elif num_digits == 2:
+                    before = " "
+                    after = "  "
+                elif num_digits == 3:
+                    before = " "
+                    after = " "
+                else:
+                    before = ""
+                    after = " "
+
+                row_str += before + self.color_codes[j] + str(j) + self.color_codes[0] + after
+            row_str += "|\n"
+            board_string += row_str
+        board_string += "   -    -    -    -  "
+
+        return board_string
+
+
+    def move(self, move):
+
+        """
+        Makes a move, valid move is 0,1,2,3
+        0 - move tiles up
+        1 - move tiles right
+        2 - move tiles down
+        3 - move tiles left
+        """
+
+        self.logger.debug(f"Received move: {move}")
+        if move not in [0,1,2,3]:
+            raise ValueError("Not a valid move - valid move is 0, 1, 2, or 3")
+
+        original_board = np.copy(self.board)
+
+        for i in range(move):
+            original_board = np.rot90(original_board)
+
+        new_board = self._execute_move(original_board)
+        for i in range(4 - move):
+            new_board = np.rot90(new_board)
+
+        if self._board_has_not_moved(new_board):
+            self.logger.info("Invalid move, squares must move!")
+            return False
+
+        else:
+            self.board = new_board
+            self.new_number_generation()
+            self.logger.debug(self.board)
+            return True
+
+
+    def game_over(self):
+
+        """
+        Verify if game is over or not
+
+        Two options for game to be over
+        1. No more empty spaces in board
+        2. You have a 2048 tile
+        """
+
+        if np.any(self.board == 2048):
+            self.logger.critical("2048 - You Won!")
+            return True
+
+        if not np.all(self.board != 0):
+            return False
+
+        copy_board = np.copy(self.board)
+        if np.array_equal(copy_board, self._execute_move(copy_board)):
+            copy_board = np.rot90(copy_board)
+            if np.array_equal(copy_board, self._execute_move(copy_board)):
+                copy_board = np.rot90(copy_board)
+                if np.array_equal(copy_board, self._execute_move(copy_board)):
+                    copy_board = np.rot90(copy_board)
+                    if np.array_equal(copy_board, self._execute_move(copy_board)):
+                        self.logger.info("No more moves - You Lose!")
+                        return True
+
+        return False
+
+
+    def new_number_generation(self):
+        """
+        Generate a new number at the beginning of new turn
+        Only generates a new number in an empty space
+        """
+
+        rows, columns = np.where(self.board == 0)
+
+        new_number = random.choice([2,4])
+        index = random.choice(np.arange(len(rows)))
+        array_index = (rows[index], columns[index])
+
+        self.board[array_index] = new_number
+
+
+    def _execute_move(self, board):
+
+        new_board = np.zeros((4,4), np.int16)
+
+        for j in range(board.shape[1]):
+
+            column = board[:,j]
+            new_column = np.zeros_like(column, np.int16)
+
+            non_zero_column = column[column!=0]
+
+            for k in range(4):
+                num, non_zero_column = self._compress_column(non_zero_column)
+                new_column[k] = num
+
+            new_board[:,j] = new_column
+
+        return new_board
+
+    def _compress_column(self, column):
+
+        if len(column) == 0:
+            return 0, []
+
+        elif len(column) == 1:
+            return column[0], []
+
+        else:
+            if column[0] == column[1]:
+                return column[0] + column[1], column[2:]
+            else:
+                return column[0], column[1:]
+
+
+    def _board_has_not_moved(self, new_board):
+        return np.array_equal(self.board, new_board)
--- play.py
+++ play.py
@@ -1,1 +1,181 @@
+from game_board import GameBoard
 
+import logging
+import configparser
+import os
+import json
+import numpy as np
+import re
+
+
+def _read_json_file(filename):
+    try:
+        with open(filename, 'r') as file:
+            return json.load(file)
+    except Exception as e:
+        raise FileNotFoundError(f"Reading failed with error:\n {e}")
+
+
+class Player:
+
+    def __init__(self, player_type, logger_level = None):
+
+        self.config = configparser['Settings']
+        self.player_info = self.config['PLAYER_INFO']
+
+        self.player_type = player_type
+        if self.player_type not in self.player_info:
+            raise ValueError(f"Player type {player_type} not accepted player")
+
+        self.logger = logging.getLogger("2048_logger")
+        logging_level = self.player_info[self.player_type]['logging_level'] if not logger_level else logger_level
+        self.logger.setLevel(logging_level)
+
+        self.game_board = GameBoard(logger = self.logger)
+
+        self.max_moves = self.player_info[self.player_type]['max_moves']
+        self.num_moves = 0
+
+        self.history_directory = self.player_info[self.player_type]['history_directory']
+        if not os.path.exists(self.history_directory):
+            os.makedirs(self.history_directory)
+            self.logger.debug(f"Created directory {self.history_directory}")
+
+        self.max_version = self._read_max_version()
+        self.game_history = np.zeros((self.max_moves, 4, 4), np.int16)
+
+        self.move_dict = self.config['MOVE_DICT']
+
+    def _read_max_version(self, default_val = -1):
+
+        data = _read_json_file(self.config['MAX_VERSION_FILENAME'])
+        self.logger.debug(f"Reading in data: {data}")
+
+        if not data:
+            self.logger.warning(f"No data read, returning default value of {default_val}")
+            return default_val
+
+        if self.player_type not in data:
+            self.logger.warning(f"Player type not found in {self.config['MAX_VERSION_FILENAME']}, returning default value of {default_val}")
+            return default_val
+
+        return int(data[self.player_type])
+
+
+    def _write_max_version(self, new_version = -1, data_overwrite = None):
+
+        if data_overwrite:
+            data = data_overwrite
+        else:
+            data = _read_json_file(self.config['MAX_VERSION_FILENAME'])
+            if not isinstance(new_version, int):
+                new_version = int(new_version)
+            data[self.player_type] = new_version
+
+        with open(self.config['MAX_VERSION_FILENAME'], 'w') as file:
+            try:
+                json.dump(data, file)
+                return data
+            except:
+                raise ValueError(f"Data {data} failed to write to file {file}")
+
+
+    def _reset_max_version(self):
+
+        if self.player_type != 'admin':
+            self.logger.error(f"Player type {self.player_type} doesn't have permission to use this method!")
+            return None
+
+        max_version_data = {}
+        pattern = r'\d+'
+
+        for player, settings in self.player_info.items():
+            self.logger.debug(f"Getting game info for player {player}")
+            directory = settings['history_directory']
+
+            game_list = [entry for entry in os.listdir(directory) if entry.endswith('.json')]
+
+            if not game_list:
+                self.logger.debug(f"No games found in directory {directory}")
+                max_version_data[player] = 0
+                continue
+
+            self.logger.debug(f"Found {len(game_list)} games in directory {directory}")
+
+            game_nums = [int(re.findall(pattern, game)[0]) for game in game_list]
+            self.logger.debug(f"Extracted {len(game_nums)} game numbers from game list")
+
+            max_version_data[player] = max(game_nums)
+            self.logger.debug(f"Found max game number {max_version_data}")
+
+        self._write_max_version(data_overwrite=max_version_data)
+        return max_version_data
+
+
+
+    def _save_game_data(self):
+
+        file_name = self.history_directory + f'game_{self.max_version + 1}.json'
+        json_data = self._get_game_metrics()
+
+        try:
+            with open(file_name, 'w') as file:
+                json.dump(json_data, file)
+
+            self.logger.info(f"Saved file at {file_name}")
+            self._write_max_version(self.max_version + 1)
+
+        except Exception as e:
+            self.logger.critical(f"Saving file {file_name} failed with error\n: {e}")
+
+    def _game_is_over(self):
+        return self.game_board.game_over() or self.num_moves == self.max_moves
+
+
+    def _update_game_info(self):
+
+        self.game_history[self.num_moves] = self.game_board.board
+        self.num_moves += 1
+
+
+    def _get_game_metrics(self):
+
+        game_metrics = {}
+
+        for name, settings in self.config['GAME_METRICS'].items():
+            self.logger.debug(f"Calculating metric {name}")
+            metric = settings['func'](self)
+            self.logger.debug(f"Returned metric: {metric}")
+
+            game_metrics[name] = metric
+        return game_metrics
+
+
+    def _get_player_game_history(self):
+
+        directory = self.history_directory
+        game_list = [os.path.join(directory, entry) for entry in os.listdir(directory) if entry.endswith('.json')]
+
+        num_games = len(game_list)
+        metric_lists = {name: np.zeros(num_games) for name,settings in self.config['GAME_METRICS'].items
+                        if settings["is_graphable"]}
+
+        for i,game in enumerate(game_list):
+            data = _read_json_file(game)
+
+            for name, metric_list in metric_lists.items():
+                metric_list[i] = data[name]
+
+        return metric_list
+
+
+    def _set_game_board(self, board):
+        self.game_board.board = board
+
+
+    def _reset(self):
+
+        self.game_board = GameBoard(logger = self.logger)
+        self.game_history = np.zeros((self.max_moves, 4, 4), np.int16)
+        self.max_version = self._read_max_version()
+        self.num_moves = 0
